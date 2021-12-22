1ba99c9d6f83b14618c3eef3a94b1675f5e246f6
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2021-12-22 17:13:06 +0530

    learning webpack

---
+++ React-1-webpack-babel-jsx/class1/src/index.js
@@ -0,0 +1,18 @@
+// function add(first, second) {
+//     return first + second
+// }
+
+// function mult(first, second) {
+//     return first * second
+// }
+
+ import {add, mult} from "./calc.js"
+ import ("./index.css")
+console.log(add(5, 15))
+console.log(mult(3, 5))
+
+const h1 = document.createElement("h1")
+
+h1.innerText = "Hello WebPack"
+h1.classList.add("redcolor");
+document.getElementById("root").appendChild(h1)
