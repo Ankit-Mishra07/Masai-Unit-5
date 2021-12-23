101f0b5cf0602116488554cff98cfd643d4388bf
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2021-12-23 16:55:22 +0530

    learning react

--- React-1-webpack-babel-jsx/class1/src/index.js
+++ React-1-webpack-babel-jsx/class1/src/index.js
@@ -6,13 +6,30 @@
 //     return first * second
 // }
 
- import {add, mult} from "./calc.js"
+//  import {add, mult} from "./calc.js"
  import "./index.css"
-console.log(add(5, 15))
-console.log(mult(3, 5))
+import React from "react"
+import ReactDom from "react-dom"
 
-const h1 = document.createElement("h1")
+// const h1 = document.createElement("h1")
 
-h1.innerText = "Hello WebPack"
-h1.classList.add("redcolor");
-document.getElementById("root").appendChild(h1)
+// h1.innerText = "Hello WebPack"
+// h1.classList.add("redcolor");
+// document.getElementById("root").appendChild(h1)
+
+// ReactDom.render(
+//     React.createElement("h1", {className : "first"}, [
+//         "Hello webpack ",
+//         React.createElement("i", null, "React"),
+//     ]),
+//     document.getElementById("root")
+// )
+
+ReactDom.render(
+    <h1 className="first">Hello
+    <i>React</i>
+    </h1>
+    ,
+
+    document.getElementById("root")
+)
