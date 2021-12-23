6b1e6f954963ad01daaa2ccd6902faeea029d8df
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2021-12-23 23:11:20 +0530

    Assignment done

--- React-2-react-introduction/Assignment/react-card/src/App.js
+++ React-2-react-introduction/Assignment/react-card/src/App.js
@@ -1,25 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
-
+import System from './system';
+import Manufactureres from './manufactureres';
 function App() {
   return (
     <div className="App">
-     <div className="card1">
-       <h1>Mobile Operating System</h1>
-       <ul>
-         <li>Android</li>
-         <li>Blackberry</li>
-         <li>iPhone</li>
-         <li>Windows Phone</li>
-       </ul>
-     </div>
-     <div className="card2">
-       <h1>Mobile Manufacturers</h1>
-       <li>Samsung</li>
-       <li>HTC</li>
-       <li>Micromax</li>
-       <li>Apple</li>
-     </div>
+     
+     <System />
+     <Manufactureres />
     </div>
   );
 }
