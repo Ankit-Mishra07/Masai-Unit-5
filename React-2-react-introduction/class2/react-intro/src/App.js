0a0b823596156c162fcb35821d4fbaae46426036
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2021-12-23 17:19:28 +0530

    Reusability

--- React-2-react-introduction/class2/react-intro/src/App.js
+++ React-2-react-introduction/class2/react-intro/src/App.js
@@ -3,15 +3,27 @@ import './App.css';
 
 function App() {
 
+  const arr = [1, 2, 3, 4, 5]
   
   return (
+
     <div className="App">
-      <div style={{color: "blue"}}>{[1, 2, 3, 4, 5].map((e) => (
+      {Todos()}
+      <Todos />
+      <div style={{color: "blue"}}>{arr.map((e) => (
         <h1>Hello {e}</h1>
       ))} </div>
     
     </div>
   );
+}
+
+function Todos() {
+  return (
+    <>
+    <h1>Hi Ankit</h1>
+    </>
+  )
 }
 
 export default App;
