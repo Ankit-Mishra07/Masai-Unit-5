99046e2cffc706f62bf3b07b5407e0016eea2d18
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2021-12-23 17:02:39 +0530

    Hello React

--- React-2-react-introduction/class2/react-intro/src/App.js
+++ React-2-react-introduction/class2/react-intro/src/App.js
@@ -2,10 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const sty = {
+    color : "green",
+    fontSize : "40px"
+  }
   return (
     <div className="App">
       
-      <h1>Hello React</h1>
+      <h1 style={{color: "red"}}>Hello React</h1>
+      <h1 style={sty}>Hello React Again</h1>
     
     </div>
   );
