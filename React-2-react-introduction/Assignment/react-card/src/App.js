7aee89a63e7f8ce5ed7ba3de6e31c565e6df41d3
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2021-12-23 20:26:45 +0530

    Assignment done

---
+++ React-2-react-introduction/Assignment/react-card/src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <div className="card1">
+       <h1>Mobile Operating System</h1>
+       <ul>
+         <li>Android</li>
+         <li>Blackberry</li>
+         <li>iPhone</li>
+         <li>Windows Phone</li>
+       </ul>
+     </div>
+     <div className="card2">
+       <h1>Mobile Manufacturers</h1>
+       <li>Samsung</li>
+       <li>HTC</li>
+       <li>Micromax</li>
+       <li>Apple</li>
+     </div>
+    </div>
+  );
+}
+
+export default App;
