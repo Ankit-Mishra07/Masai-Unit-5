f90b2027426f20824775302fcaca342ba1bc93bb
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2021-12-24 15:16:53 +0530

    preclass

--- React-3-usestate-state-props/Preclass/learnprops/src/App.js
+++ React-3-usestate-state-props/Preclass/learnprops/src/App.js
@@ -1,19 +1,15 @@
 import React from 'react'
 import './App.css';
+import Counter from './counter/Counter';
+
+import { Task } from './Input/Task';
 
 function App() {
-  const [counter, setCounter] = React.useState(5)
 
-  const handleIncrement = (value) => {
-    setCounter(counter + value)
-  }
 
   return (
     <div className="App">
-     <h1>Counter</h1>
-     <h2>{counter}</h2>
-     <button onClick={() => handleIncrement(1)}>ADD</button>
-     <button onClick={() => handleIncrement(-1)}>REDUCE</button>
+    <Task />
     </div>
   );
 }
