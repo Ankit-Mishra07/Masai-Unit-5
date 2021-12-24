7f2bc2496eddb1117e2fb56c3509ceff1ca50494
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2021-12-24 17:16:05 +0530

    learning useState

--- React-3-usestate-state-props/Preclass/learnprops/src/App.js
+++ React-3-usestate-state-props/Preclass/learnprops/src/App.js
@@ -1,15 +1,21 @@
 import React from 'react'
 import './App.css';
-import Counter from './counter/Counter';
+// import Counter from './counter/Counter';
 
-import { Task } from './Input/Task';
-
+// import { Task } from './Input/Task';
+import Todo from './component/Todo';
 function App() {
 
+  const arr = ["wake" , "code10", "code"]
 
   return (
     <div className="App">
-    <Task />
+      {arr.map((e) => (
+          <Todo num={e}> 
+          <h1 className='child'>ChildrenH</h1>
+          <h1 className='child'>ChildrenH2</h1>
+          </Todo>
+      ))}
     </div>
   );
 }
---
+++ React-3-usestate-state-props/usestateclass/src/App.jsx
@@ -0,0 +1,33 @@
+import './App.css';
+
+import {useState} from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const addOne = (value) => {
+    // if(count < 15) {
+    //   setCount(count + value)
+    // }
+    setCount(count + value)
+  }
+
+  if(count > 15) {
+    return <div>Counter Reached max</div>
+  }
+  return (
+    <div className="App">
+      <h3>Counter : {count} </h3>
+      {count < 25 && (
+        <div>
+          <button onClick={() => addOne(1)}>ADD ONE</button>
+      <button onClick={() => addOne(-1)}>SUBSTRACT ONE</button>
+      <div>Counter is {count % 2 === 0 ? "Even" : "Odd"}</div>
+        </div>
+      )} 
+      
+    </div>
+  );
+}
+
+export default App;
