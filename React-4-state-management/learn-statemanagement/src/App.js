2640875550b1386811565576459b0627c880d404
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2021-12-27 14:23:49 +0530

    ParenttoChild and ChildtoParent relation

--- React-4-state-management/learn-statemanagement/src/App.js
+++ React-4-state-management/learn-statemanagement/src/App.js
@@ -1,21 +1,13 @@
 import './App.css';
+import ChildtoParent from './Preclass/ParenttoChild';
+import ParenttoChild from './Preclass/ParenttoChild';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ParenttoChild />
+      <ChildtoParent />
     </div>
   );
 }
