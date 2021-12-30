b8bd52e92c6d7ea08ffbd19ef07dbc548e5a8663
Ankit Mishra <90400569+Ankit-Mishra07@users.noreply.github.com>
2021-12-30 18:13:36 +0530

    Timer

--- React-6-useeffects/learn-effect/src/App.js
+++ React-6-useeffects/learn-effect/src/App.js
@@ -1,18 +1,31 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import "./App.css"
+import Timer from './Assignment/Timer'
 
-import Todo from './Class/Todo'
 
 
 const App = () => {
+
+    const [show, setShow] = useState(false)
+
+    // const [start, setStart] = useState(0)
+    // const [end, setEnd] = useState(10)
+
     return (
-
-        <div className='App'>
+            <div className='App'>
             
-        <Todo />
 
-        </div>
-    )
-}
+           {show ? <Timer start={0} end={10} /> : null}
+            <button onClick={() => setShow(!show)}>{show ? "Hide" : "Show"}</button>
+            </div>
+        )
+    }
 
 export default App
+
+// <div className='App'>
+    
+//     {show ? <Timer /> : null}
+
+// <button onClick={() => setShow(!show)}>{show ? "Hide" : "Show"}</button>
+// </div>
--- React-6-useeffects/learn-effect/src/Class/Todo.jsx
+++ React-6-useeffects/learn-effect/src/Class/Todo.jsx
@@ -3,16 +3,17 @@ import React, {useEffect, useState} from "react";
 const Todo = () => {
   const [text, setText] = useState("");
   const [todos, setTodos] = useState([])
+  const [page, setPage] = useState(1)
 
   const getTodos = () => {
-      fetch("http://localhost:3003/todos")
+      fetch(`http://localhost:3003/todos?_page=${page}&_limit=2`)
       .then(d => d.json())
       .then((res) => setTodos(res))
   }
 
   useEffect(() => {
     getTodos()
-  })
+  },[page])
 
   const addTodo= () => {
     const payload = {
@@ -48,6 +49,10 @@ const Todo = () => {
               <div key={i}>{e.title}</div>
           ))
       }
+      <div>
+        <button disabled={page == 1} onClick={() => setPage(page - 1)}>PREV</button>
+        <button onClick={() => setPage(page + 1)}>NEXT</button>
+      </div>
     </>
   );
 };
